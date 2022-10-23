15aaf199857d3b8b65b9dab05612a5c37c6a418f
Stefan Hagen <stefan@hagen.link>
2022-10-23 22:06:00 +0200

    Adapted tests

--- test/test_kiirastuli.py
+++ test/test_kiirastuli.py
@@ -1,5 +1,9 @@
-from kiirastuli.kiirastuli import parse
+import kiirastuli.kiirastuli as api
 
 
-def test_parse():
-    assert parse() is NotImplemented
+def test_humanize_mass():
+    assert api.humanize_mass(0) == ('0', 'total bytes')
+
+
+def test_humanize_duration():
+    assert api.humanize_duration(0) == ('0.000', 'millis')
